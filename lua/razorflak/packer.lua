93968b86678a5951c6b64c82def9b4322dd15b3e
Julien T <tanguyj35@gmail.com>
2023-08-24 23:45:49 +0200

    ajout d'une status bar

--- lua/razorflak/packer.lua
+++ lua/razorflak/packer.lua
@@ -6,7 +6,10 @@ vim.cmd.packadd('packer.nvim')
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         -- or                            , branch = '0.1.x',
