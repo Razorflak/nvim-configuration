2a7835641d6cac5818257f3beac0a42e00a57d95
Julien T <tanguyj35@gmail.com>
2023-08-29 23:56:50 +0200

    Ajout de codeium

--- lua/razorflak/packer.lua
+++ lua/razorflak/packer.lua
@@ -98,4 +98,15 @@ return require('packer').startup(function(use)
         use("eandrju/cellular-automaton.nvim")
         use("laytan/cloak.nvim")
         use("m4xshen/autoclose.nvim")
+        use {
+            'Exafunction/codeium.vim',
+            config = function ()
+                -- Change '<C-g>' here to any keycode you like.
+                print("jta")
+                vim.keymap.set('i', '<C-i>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+                vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+                vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+                vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+            end
+        }
     end)
