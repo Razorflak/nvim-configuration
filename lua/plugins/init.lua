e60a9bc7ed6e9a12ac80921c04554c270e444fa0
razorflak <tanguyj35@gmail.com>
2023-10-18 17:57:07 +0200

    LazyGit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,22 +1,21 @@
 return {
     -- Packer can manage itself
-    
     {
-	    "kylechui/nvim-surround",
-	    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	    event = "VeryLazy",
-	    config = function()
-		    require("nvim-surround").setup({
-			    -- Configuration here, or leave empty to use defaults
-		    })
-	    end
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
     },
     {
-	    'rose-pine/neovim',
-	    as = 'rose-pine',
-	    config = function()
-		    vim.cmd('colorscheme rose-pine')
-	    end
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
     }, 
 
     {
@@ -37,21 +36,18 @@ return {
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
         end
-        },
+    },
 
-        "christoomey/vim-tmux-navigator",
-        "sindrets/diffview.nvim",
-        "nvim-treesitter/playground",
-        "theprimeagen/refactoring.nvim",
-        "theprimeagen/harpoon",
-        "mbbill/undotree",
-        "tpope/vim-fugitive",
-        "nvim-treesitter/nvim-treesitter-context",
-        "dense-analysis/ale",
-        
-         
-
-        "folke/zen-mode.nvim",
-        "eandrju/cellular-automaton.nvim",
-        "laytan/cloak.nvim",
-    }
+    "christoomey/vim-tmux-navigator",
+    "sindrets/diffview.nvim",
+    "nvim-treesitter/playground",
+    "theprimeagen/refactoring.nvim",
+    "theprimeagen/harpoon",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    "nvim-treesitter/nvim-treesitter-context",
+    "dense-analysis/ale",
+    "folke/zen-mode.nvim",
+    "eandrju/cellular-automaton.nvim",
+    "laytan/cloak.nvim",
+}
