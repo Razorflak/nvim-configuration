e88d13fe16b7544cdd3cfe82ea6325b2787c17b2
Julien T <tanguyj35@gmail.com>
2023-12-21 00:26:15 +0100

    Correction de la conf tree sitter et ajout de telescope pour les code actions

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -21,13 +21,10 @@ return {
     },
     config = function()
         local lsp = require("lsp-zero")
-
         lsp.preset("recommended")
-
         lsp.ensure_installed({
             'tsserver',
         })
-
         -- Fix Undefined global 'vim'
         lsp.nvim_workspace()
 
