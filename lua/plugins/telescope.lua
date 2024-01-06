083e9f0584f72e6c6ee8df0297bf8e5c6567f6f3
Julien T <tanguyj35@gmail.com>
2024-01-06 03:09:14 +0100

    Utilisation de trouble pour les références, recherche de la sélection dans telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,17 @@
+function vim.getVisualSelection()
+    vim.cmd('noau normal! "vy"')
+    local text = vim.fn.getreg("v")
+    vim.fn.setreg("v", {})
+
+    ---@diagnostic disable-next-line: param-type-mismatch
+    text = string.gsub(text, "\n", "")
+    if #text > 0 then
+        return text
+    else
+        return ""
+    end
+end
+
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
@@ -10,5 +24,9 @@ return {
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        vim.keymap.set("v", "<leader>fv", function()
+            local text = vim.getVisualSelection()
+            builtin.grep_string({ search = text })
+        end)
     end,
 }
