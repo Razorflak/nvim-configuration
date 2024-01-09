58c93e98a4b959b4bad92cae2524dc57cafa6662
Julien T <tanguyj35@gmail.com>
2024-01-09 23:29:06 +0100

    Passage à copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,45 @@
 return {
-    {
-        "github/copilot.vim",
-    },
+    "zbirenbaum/copilot.lua",
+    config = function()
+        require('copilot').setup({
+            panel = {
+                enabled = true,
+                auto_refresh = true,
+                keymap = {
+                    jump_prev = "[[",
+                    jump_next = "]]",
+                    accept = "<CR>",
+                    refresh = "gr",
+                    open = "<M-CR>"
+                },
+                layout = {
+                    position = "bottom", -- | top | left | right
+                    ratio = 0.4
+                },
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+                keymap = {},
+            },
+            filetypes = {
+                yaml = false,
+                markdown = false,
+                help = false,
+                gitcommit = false,
+                gitrebase = false,
+                hgcommit = false,
+                svn = false,
+                cvs = false,
+                ["."] = false,
+            },
+            copilot_node_command = 'node', -- Node.js version must be > 18.x
+            server_opts_overrides = {},
+        })
+
+		vim.keymap.set("i", "<tab>", function()
+			require("copilot.suggestion").accept_line()
+		end)
+    end
 }
