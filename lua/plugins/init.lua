cc327001eb8ed2737a22d4437f0b00933a352aa3
Julien T <tanguyj35@gmail.com>
2024-04-20 01:17:06 +0200

    extration de surround de l'init

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,32 +1,25 @@
 return {
-    -- Packer can manage itself
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require("nvim-surround").setup {}
-        end,
-    },
-    {
-        "rose-pine/neovim",
-        as = "rose-pine",
-        config = function()
-            vim.cmd("colorscheme rose-pine")
-        end,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = function()
-            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-            ts_update()
-        end,
-    },
+	-- Packer can manage itself
+	{
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	},
 
-    "christoomey/vim-tmux-navigator",
-    "theprimeagen/refactoring.nvim",
-    "mbbill/undotree",
-    "tpope/vim-fugitive",
-    "nvim-treesitter/nvim-treesitter-context",
-    "eandrju/cellular-automaton.nvim",
-    "dyng/ctrlsf.vim",
+	"christoomey/vim-tmux-navigator",
+	"theprimeagen/refactoring.nvim",
+	"mbbill/undotree",
+	"tpope/vim-fugitive",
+	"nvim-treesitter/nvim-treesitter-context",
+	"eandrju/cellular-automaton.nvim",
+	"dyng/ctrlsf.vim",
 }
