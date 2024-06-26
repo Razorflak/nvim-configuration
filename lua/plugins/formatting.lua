00fdaccc0c52c9679f29eb9e15bfbfa3b0a8b809
razorflak <tanguyj35@gmail.com>
2024-06-26 09:57:00 +0200

    Augmentation du timeout sur le formattage

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -40,7 +40,7 @@ return {
 			format_on_save = {
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 3000,
 			},
 			notify_on_error = false,
 		})
@@ -49,7 +49,7 @@ return {
 			conform.format({
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 3000,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,
