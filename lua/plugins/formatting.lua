5c344c0778234dacd179cae8946b961f70ed1d9a
razorflak <tanguyj35@gmail.com>
2024-06-26 08:57:57 +0200

    formatter paramétrable par projet

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,45 +1,56 @@
-return {
-    "stevearc/conform.nvim",
-    config = function()
-        local conform = require("conform")
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "biome", "prettier" },
-                -- typescript = {"biome", "prettier"},
-                javascriptreact = { "biome", "prettier" },
-                typescriptreact = { "biome", "prettier" },
-                typescript = function()
-                    local root_dir = vim.fn.getcwd()
-                    if vim.fn.filereadable(root_dir .. '/.prettierrc') == 1 or vim.fn.filereadable(root_dir .. '/.prettierrc.js') == 1 then
-                        return { "prettier" }
-                    else
-                        return { "biome" }
-                    end
-                end,
-                svelte = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                markdown = { "prettier" },
-                graphql = { "prettier" },
-                lua = { "stylua" },
-                python = { "isort", "black" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            },
-            notify_on_error = false,
-        })
+local project_formatters = {
+	{ path = "neomed.git/eslintv2", formatter = "eslint_d" },
+	{ path = "stickycom.git", formatter = "eslint_d" },
+	{ path = "neomed.git", formatter = "prettier" },
+}
 
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+local function get_formatter(default_formatter)
+	local root_dir = vim.fn.getcwd()
+	for _, project in ipairs(project_formatters) do
+		if string.find(root_dir, project.path) then
+			return { project.formatter }
+		end
+	end
+	return { default_formatter }
+end
+
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "biome", "prettier" },
+				-- typescript = {"biome", "prettier"},
+				javascriptreact = { "biome", "prettier" },
+				typescriptreact = { "biome", "prettier" },
+				typescript = function()
+					return get_formatter("prettier")
+				end,
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+			notify_on_error = false,
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
